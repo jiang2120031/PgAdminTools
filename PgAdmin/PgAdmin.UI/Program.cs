198b67c908f075a19e8096f5256732c88f55dc8a
Olivia <cuicui.zhu@feisystems.com>
2018-03-26 11:26:34 +0800

    cancel test line

--- PgAdmin/PgAdmin.UI/Program.cs
+++ PgAdmin/PgAdmin.UI/Program.cs
@@ -16,8 +16,7 @@ namespace PgAdmin.UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-            /////////
+            Application.Run(new Form1());           
         }
     }
 }
